505444e4d023040e5245e5b1e0321aa6de494900
wearshoes <xwmrxwmr@qq.com>
2021-11-25 11:57:58 +0800

    【提交工程及初始化】

---
+++ Assets/Scripts/AudioSpeedControl.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSpeedControl : MonoBehaviour
+{
+
+    /// <summary>
+    /// 目前只能处理0.5-2倍速的变速播放
+    /// </summary>
+
+
+    public AudioSource audio;
+    public Slider pitchSlider;
+    public Button playBtn;
+    public Text sliderText;
+
+    private AudioMixer mixer;
+
+
+    private void Awake()
+    {
+        InitSlider();
+        sliderText.text = "1";
+        playBtn.onClick.AddListener(OnClickPlayBtn);
+       
+
+        if (audio!=null&&audio.outputAudioMixerGroup!=null)
+        {
+            mixer = audio.outputAudioMixerGroup.audioMixer;
+        }
+    }
+
+    private void InitSlider()
+    {
+        pitchSlider.maxValue = 2f;
+        pitchSlider.minValue = 0.5f;
+        pitchSlider.value = 1f;
+        pitchSlider.onValueChanged.AddListener(OnSliderValueChanged);
+    }
+
+    private void SetMixerPitch(float value)
+    {
+        if (mixer == null) return;
+        var pitchValue = 1f / value;
+        mixer.SetFloat("MixerPitch", pitchValue);
+    }
+
+    private void OnSliderValueChanged(float value)
+    {
+        sliderText.text = value.ToString();
+        audio.pitch = value;
+        SetMixerPitch(value);
+
+    }
+
+    private void OnClickPlayBtn()
+    {
+        if (audio==null)return;
+        audio.Play();
+    }
+
+
+}
